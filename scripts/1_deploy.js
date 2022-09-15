d96d9c19051973cf88c90132b032f58e0f73f497
Sankhadip Pal <88395297+RonWizz@users.noreply.github.com>
2022-09-15 23:30:14 +0530

    Update 1_deploy.js
    Contracts are deployed

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -9,7 +9,24 @@ async function main() {
   const accounts = await ethers.getSigners()
 
   console.log(`Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}\n`)
+  
+  // Deploy contracts
+  const dapp = await Token.deploy('Dapp University', 'DAPP', '1000000')
+  await dapp.deployed()
+  console.log(`DAPP Deployed to: ${dapp.address}`)
 
+  const mETH = await Token.deploy('mETH', 'mETH', '1000000')
+  await mETH.deployed()
+  console.log(`mETH Deployed to: ${mETH.address}`)
+
+  const mDAI = await Token.deploy('mDAI', 'mDAI', '1000000')
+  await mDAI.deployed()
+  console.log(`mDAI Deployed to: ${mDAI.address}`)
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10)
+  await exchange.deployed()
+  console.log(`Exchange Deployed to: ${exchange.address}`)
+}
 
 
 main()
